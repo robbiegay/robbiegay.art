777df5319d5e7e6ad93f00d06e4e61ec625cb5c9
Robbie Gay <robbiecgay@gmail.com>
2020-02-28 13:04:48 -0500

    Added nav bar to all pages

--- src/Blog.js
+++ src/Blog.js
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import NavBar from './NavBar'
+import NavBar from './NavBar';
 
 function Blog() {
-    const [time, setTime] = useState(new Date());
-    const settings = { hour: 'numeric', minute: '2-digit' };
+    // const [time, setTime] = useState(new Date());
+    // const settings = { hour: 'numeric', minute: '2-digit' };
 
-    useEffect(() => {
-        const clock = setInterval(
-            () => setTime(new Date()), 1000
-        );
+    // useEffect(() => {
+    //     const clock = setInterval(
+    //         () => setTime(new Date()), 1000
+    //     );
 
-        return function cleanup() {
-            clearInterval(clock);
-        }
-    });
+    //     return function cleanup() {
+    //         clearInterval(clock);
+    //     }
+    // });
 
     return (
         <>
             <NavBar />
-            <h1 className='clockFont text-white'>{time.toLocaleTimeString([], settings)}</h1>
             <h1>Blog Page</h1>
             <h1>Blog Page</h1>
             <h1>Blog Page</h1>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,22 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import NavBar from './NavBar';
 
 function Portfolio() {
-    const [time, setTime] = useState(new Date());
-    const settings = { hour: 'numeric', minute: '2-digit' };
-
-    useEffect(() => {
-        const clock = setInterval(
-            () => setTime(new Date()), 1000
-        );
-
-        return function cleanup() {
-            clearInterval(clock);
-        }
-    });
-
     return (
         <>
-            <h1 className='clockFont text-white'>{time.toLocaleTimeString([], settings)}</h1>
+            <NavBar />
             <h1>Portfolio Page</h1>
         </>
     );
