53b6b581b5971722fc39cb9152d0b85a4507e9c4
Robbie Gay <robbiecgay@gmail.com>
2020-02-27 19:45:15 -0500

    Updates

---
+++ precache-manifest.a7ae90512e1fa72438c620fa9593836d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abffb3126b0c5dbe8506574f7eb909a3",
+    "url": "/robbiegay/robbiegay.art/settings/index.html"
+  },
+  {
+    "revision": "21e2972af85d87d47c6d",
+    "url": "/robbiegay/robbiegay.art/settings/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17ef2ef0ab87a1108dc0",
+    "url": "/robbiegay/robbiegay.art/settings/static/js/2.5c98bb4b.chunk.js"
+  },
+  {
+    "revision": "fbd53d2de226460fa00bbaed607fa893",
+    "url": "/robbiegay/robbiegay.art/settings/static/js/2.5c98bb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e2972af85d87d47c6d",
+    "url": "/robbiegay/robbiegay.art/settings/static/js/main.84ece242.chunk.js"
+  },
+  {
+    "revision": "49ebff3e8ccf2bdb01e9",
+    "url": "/robbiegay/robbiegay.art/settings/static/js/runtime-main.5e76ca34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robbiegay/robbiegay.art/settings/static/media/logo.5d5d9eef.svg"
+  }
+]);
