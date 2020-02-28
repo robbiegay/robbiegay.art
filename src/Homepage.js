e4a255715011d4d3093313e9011fe273b3367442
Robbie Gay <robbiecgay@gmail.com>
2020-02-27 20:57:04 -0500

    Added nav buttons, favicon, website page title, and setup github pages hosting

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PtsCanvas } from 'react-pts-canvas';
-import { Color, Create, Pt, Num, Rectangle } from 'pts';
+import { Color, Create, Pt, Num, Rectangle, UIButton } from 'pts';
 
 class Homepage extends PtsCanvas {
 
@@ -17,8 +17,9 @@ class Homepage extends PtsCanvas {
         // get LAB color string, given a point position
         let color = (p) => {
             let p1 = p.$divide(space.size);
-            let p2 = space.pointer.$divide(space.size); // [0.712290346622467, 0.668083667755127]
-            let c1 = cu.$multiply(Pt.make(4, 1).to(p2.x, p1.x - 0.5, p1.y - 0.5));
+            let p2 = space.pointer.$divide(space.size);
+            let colorIntensity = 0.65 - (p2.y * 0.3)
+            let c1 = cu.$multiply(Pt.make(4, 1).to(colorIntensity, p1.x - 0.5, p1.y - 0.5));
             return Color.LABtoRGB(c1).toString("rgb");
         }
 
@@ -29,11 +30,31 @@ class Homepage extends PtsCanvas {
         }
         
         // Textbox
-        this.form.font(60);
-        let rect = Rectangle.fromCenter( space.center, 300  ); //size.$max( space.size.$divide(6) )
-        
-        this.form.fill("rgba(0,0,0,0)").stroke("rgba(0,0,0,0)", 3).rect( rect );
-        this.form.fillOnly("#FFF").alignText("center").paragraphBox( rect, "robbiegay", 1, "middle", false );
+        // this.form.font(60);
+        let centerX = space.size.x / 2
+        let centerY = space.size.y / 2
+        let yPerc = space.size.y / 100
+
+        let nameRect = Rectangle.fromCenter( [centerX,centerY - (yPerc * 15)], 400  ); //size.$max( space.size.$divide(6) )
+        this.form.fill("rgba(0,0,0,0)").font(75).stroke("rgba(0,0,0,0)", 3).rect( nameRect );
+        this.form.fillOnly("#FFF").alignText("center").paragraphBox( nameRect, "robbiegay", 1, "middle", false );
+
+        let blogRect = Rectangle.fromCenter( [centerX,centerY + (yPerc * 10)], 300  );
+        this.form.fill("rgba(0,0,0,0)").font(40).stroke("rgba(0,0,0,0)", 3).rect( blogRect );
+        this.form.fillOnly("#FFF").alignText("center").paragraphBox( blogRect, "dev blog", 1, "middle", false );
+        // let button = UIButton.fromCircle( [10, [10,10]] );
+        // button.onClick()
+        // this.form.fill("rgba(0,0,0,0)").font(40).stroke("rgba(0,0,0,0)", 3).rect( button );
+        // this.form.fillOnly("#FFF").alignText("center").paragraphBox( button, "dev blog", 1, "middle", false );
+
+
+        let portfolioRect = Rectangle.fromCenter( [centerX,centerY + (yPerc * 20)], 300  );
+        this.form.fill("rgba(0,0,0,0)").font(40).stroke("rgba(0,0,0,0)", 3).rect( portfolioRect );
+        this.form.fillOnly("#FFF").alignText("center").paragraphBox( portfolioRect, "portfolio", 1, "middle", false );
+
+        let musicRect = Rectangle.fromCenter( [centerX,centerY + (yPerc * 30)], 300  );
+        this.form.fill("rgba(0,0,0,0)").font(40).stroke("rgba(0,0,0,0)", 3).rect( musicRect );
+        this.form.fillOnly("#FFF").alignText("center").paragraphBox( musicRect, "music", 1, "middle", false );
     }
 
     start(bound, space) {
@@ -42,3 +63,5 @@ class Homepage extends PtsCanvas {
 }
 
 export default Homepage
+
+// make text clickable
