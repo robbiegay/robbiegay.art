08b095f72f07ce9b85e20c993bc8448d7ec30eb4
Robbie Gay <robbiecgay@gmail.com>
2020-03-01 01:30:52 -0500

    Added resume pdf button, linkedin and github links, and soundcloud icon with link on music page

--- src/Music.js
+++ src/Music.js
@@ -3,9 +3,12 @@ import NavBar from './NavBar';
 import Footer from './Footer'
 
 function Music() {
+    let largerSize = "75px";
+
     return (
         <>
             <NavBar />
+            <a href="https://soundcloud.com/robbiegay" title="My SoundCloud Page" target="_blank" rel="noopener noreferrer"><i class="fab fa-soundcloud" style={{ color: "#ff6c02", fontSize: largerSize }}></i></a>
             <h1>Music Page</h1>
             <h1>Music Page</h1>
             <h1>Music Page</h1>
